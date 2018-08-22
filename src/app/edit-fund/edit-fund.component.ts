bd439b9eef071a7b36a0f11da4feff882bd1be29
Kelli Brooke and Ryan Putman <kelli@>
2018-08-22 16:27:41 -0700

    add level and delete level functions

--- src/app/edit-fund/edit-fund.component.ts
+++ src/app/edit-fund/edit-fund.component.ts
@@ -13,6 +13,7 @@ import { CrowdfundService } from '../crowdfund.service';
 export class EditFundComponent implements OnInit {
   fundId: string = null;
   fund;
+  tempLevelArray = [];
 
   constructor(private router: Router, private route: ActivatedRoute, private location: Location, private crowdfundService: CrowdfundService) { }
 
@@ -20,7 +21,12 @@ export class EditFundComponent implements OnInit {
     this.route.params.forEach((urlParameters) => {
       this.fundId = urlParameters['id'];
     });
+
     this.fund = this.crowdfundService.getFundById(this.fundId)
+    this.fund.subscribe(dataLastEmittedFromObserver => {
+      this.tempLevelArray = dataLastEmittedFromObserver.levels;
+    });
+
   }
 
   saveFundChanges(title: string, goalAmount: string, description: string, contact: string, category: string) {
@@ -30,7 +36,8 @@ export class EditFundComponent implements OnInit {
       goalAmount: goalAmount,
       description: description,
       contact: contact,
-      category: category
+      category: category,
+      levels: this.tempLevelArray
     });
     this.router.navigate(['funds', this.fundId]);
   }
@@ -42,14 +49,23 @@ export class EditFundComponent implements OnInit {
   }
 
   deleteLevel(level) {
-    let fundInDatabase;
-    this.crowdfundService.getFundById(this.fundId).subscribe(dataLastEmittedFromObserver => {
-      fundInDatabase = dataLastEmittedFromObserver;
-    });
-    let levelIndex = fundInDatabase.levels.indexOf(level);
-    let observableLevel = this.crowdfundService.getLevelByIndex(levelIndex, this.fundId);
-    observableLevel.remove();
-
+    let index = this.tempLevelArray.indexOf(level);
+    this.tempLevelArray.splice(index, 1);
   }
+
+  addLevel(level:string){
+  this.tempLevelArray.push(level);
+  }
+
+  // deleteLevel(level) {
+  //   let fundInDatabase;
+  //   this.crowdfundService.getFundById(this.fundId).subscribe(dataLastEmittedFromObserver => {
+  //     fundInDatabase = dataLastEmittedFromObserver;
+  //   });
+  //   let levelIndex = fundInDatabase.levels.indexOf(level);
+  //   let observableLevel = this.crowdfundService.getLevelByIndex(levelIndex, this.fundId);
+  //   observableLevel.remove();
+  //
+  // }
 
 }
